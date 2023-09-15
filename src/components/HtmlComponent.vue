18f65ea94ed9b6c67b7b1cd8d58c79c2cad81c28
waximabbax <waximabbax@gmail.com>
2023-09-15 15:03:56 +0500

    Testing Vue app with Jest

---
+++ src/components/HtmlComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+      firstName: '',
+      secondName: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HtmlComponent msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HtmlComponent from '@/components/HtmlComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HtmlComponent,
+  },
+}
+</script>
